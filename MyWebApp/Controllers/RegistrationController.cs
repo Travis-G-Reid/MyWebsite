4e0b1ef5fb71e467482bd9f021a1d862d93094d5
Travis Reid <travisreid0901@gmail.com>
2023-11-25 03:32:21 -0600

    Made skeleton page & skeleton logic for registration

---
+++ MyWebApp/Controllers/RegistrationController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebApp.Controllers
+{
+	public class RegistrationController : Controller
+	{
+		// GET: /Account/Register
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		// POST: /Account/Register
+		[HttpPost]
+		public IActionResult Register(string username, string password, string email)
+		{
+			// Your registration logic here
+
+			// After successful registration
+			return RedirectToAction("Index", "Home");
+		}
+	}
+}
