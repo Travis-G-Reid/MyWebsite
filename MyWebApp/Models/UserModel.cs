f47ef18b06f5c12d12dcd6075f23e328f962471c
Travis Reid <travisreid0901@gmail.com>
2023-11-26 01:33:08 -0600

    Add register and user model

---
+++ MyWebApp/Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MyWebApp.Models
+{
+	public class User
+	{
+		public string Username { get; set; }
+		public string PasswordHash { get; set; }
+		public string Email { get; set; }
+
+	}
+}
