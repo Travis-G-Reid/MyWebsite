9f087fa246a5f2962f8ce4a75f036e5366ffd2be
Travis Reid <travisreid0901@gmail.com>
2023-11-30 04:18:49 -0600

    Ported Search Page from another project (Large commit; review thoroughly)

---
+++ MyWebApp/Controllers/SearchController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace MyWebApp.Controllers
+{
+    [Route("Search")]
+    public class SearchController : Controller
+    {
+        [HttpGet("GetCategories")]
+        public IActionResult GetCategories()
+        {
+            var categoriesDictionary = FileParser.GetCategoriesDictionaryFromFile();
+            return Json(categoriesDictionary);
+        }
+    }
+}
